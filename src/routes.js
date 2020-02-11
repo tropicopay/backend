2f99007f21bb6960409f24ecbc7688faa41872b2
tropicopay <60882646+tropicopay@users.noreply.github.com>
2020-02-11 14:00:08 -0300

    Update routes.js
    Changing "/Users POST" route to new route with avatar upload function

--- src/routes.js
+++ src/routes.js
@@ -8,11 +8,11 @@ const PlaceController = require('./controllers/PlaceController');
 
 const routes = Router();
 
-routes.post('/Users', UserController.store);
+routes.post('/Users', upload.single('avatar'), UserController.store);
 routes.get('/Users', UserController.index);
 routes.delete('/Users', UserController.destroy);
 routes.put('/Users', UserController.update);
-routes.post('/Users', upload.single('avatar'), UserController.store);
+
 
 routes.post('/Places', PlaceController.store);
 routes.get('/Places', PlaceController.index);
