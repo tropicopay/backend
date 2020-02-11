96eb891b146b0395455636c9d5afdfff7138b1fc
tropicopay <60882646+tropicopay@users.noreply.github.com>
2020-02-11 13:49:10 -0300

    Update User.js
    Key name changed from "thumbnail" to "avatar_url"

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  thumbnail: String,
+  avatar_url: { type: String },
   name: { type: String, required: true },
   email: { type: String, required: true },
   cpf: { type: String, required: true },
