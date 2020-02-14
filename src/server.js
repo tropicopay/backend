dc797af6df9f27800ffda6677d2f9d8400ad8a8f
Gabriell Huver <tropicopayveloez@gmail.com>
2020-02-14 16:48:11 -0300

    heroku change env port const

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const path = require('path');
 const express = require('express');
 
-app.listen(process.env.SERVER_PORT || 3000);
+app.listen(process.env.PORT || 3000);
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
